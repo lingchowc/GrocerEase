0a0efe558c4f7c8a06b83a7f3a09a37cf1001dd6
Chow <csl.chowling@gmail.com>
2024-12-08 17:57:29 +0000

    Add docker

---
+++ tests/test.php
@@ -0,0 +1,18 @@
+<?php
+
+// require_once '../html/functions/dbcontest.php';
+
+class Test extends PHPUnit\Framework\TestCase
+{
+ public function testOutput()
+ {
+//     // Capture the output of hello.php
+//     ob_start();
+//     include '../html/functions/dbcontest.php';
+//     $output = ob_get_clean();
+
+//     // Assert that the output is "Hello, Docker!"
+//     $this->assertEquals("OK", $output);
+ }
+}
+?>
