80843acdd4f07609fbcd8841245db2205aa89da3
Chow <csl.chowling@gmail.com>
2024-12-09 19:05:34 +0000

    Add Docker CI

--- php/login.php
+++ php/login.php
@@ -67,7 +67,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Dashboard - Login</title>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/login.css?v=1.0">
 </head>
 <body>
     <div class="container">
